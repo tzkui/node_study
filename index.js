7197d10916627e9b10790369761fccc69d7e48d1
tzkui <993477421@qq.com>
2023-07-11 18:05:27 +0800

    tzk

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-const app = require("express")();
+const express = require("express")
+const app = express();
+const wsInstance = require('express-ws')(app);
+
+app.ws("/", ws => {
+  ws.on("message", data => {
+    wsInstance.getWss().clients.forEach(server => {
+      if (server !== ws) {
+        server.send(data)
+      }
+    })
+  })
+})
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  next();
+});
 app.get("/", (req, res) => {
   res.sendFile("./pages/index.html", { root: __dirname })
 })
-app.get("/chats",(req,res)=>{
+app.get("/chats", (req, res) => {
   res.sendFile("./pages/chats.html", { root: __dirname })
 })
 app.listen(1234)
+app.use(express.static("public"))
